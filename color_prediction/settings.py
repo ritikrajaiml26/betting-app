f24e934e45621f850fe5fa9933555ba2646b2210
Ritik Raj <ritikrajaiml@gmail.com>
2026-06-17 10:05:44 +0530

    setup postgres neon

--- color_prediction/settings.py
+++ color_prediction/settings.py
@@ -79,13 +79,19 @@ WSGI_APPLICATION = 'color_prediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+
+from dotenv import load_dotenv
+import dj_database_url
+import os
+
+load_dotenv(BASE_DIR / ".env")
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
